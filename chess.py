9e54129aba86a0cb9a54dfa80fc6d65ec8ad6a0b
NationofFoxes <stuart.william.auld@gmail.com>
2023-10-12 23:46:58 +0900

    Knight & incomplete Rook move validation checks

--- chess.py
+++ chess.py
@@ -23,31 +23,119 @@ class King(Piece):
         piece_id = color + "K"
         super().__init__(color, piece_id)
 
+    def is_valid_move(self, origin, target):
+        # TODO
+        # Write a method that returns True if there is one space between the origin and target 
+        # AND the target space is not at risk of being taken by a piece of the opposing colour
+        return True
+
 
 class Queen(Piece):
     def __init__(self, color):
         piece_id = color + "Q"
         super().__init__(color, piece_id)
 
+    def is_valid_move(self, origin, target):
+        # TODO
+        # Write a method that returns True if the path between origin and target is orthonogal or diagonal (straight lines only)
+        # AND the spaces on the travelled path are clear
+        return True
+
 class Knight(Piece):
     def __init__(self, color):
         piece_id = color + "Kn"
         super().__init__(color, piece_id)
+
+    
+    def is_valid_move(self, origin, target):
+        # Check if the move is valid for a knight
+
+        # Convert coordinates to indices (0-based)
+        origin_col, origin_row = ord(origin[0]) - ord('a'), 8 - int(origin[1])
+        target_col, target_row = ord(target[0]) - ord('a'), 8 - int(target[1])
+
+        # Check if the target position is within the bounds of the chessboard
+        if target_row < 0 or target_row >= 8 or target_col < 0 or target_col >= 8:
+            return False
+
+        # Calculate the absolute differences between the rows and columns
+        row_diff = abs(origin_row - target_row)
+        col_diff = abs(origin_col - target_col)
+
+        # Knights move in an L-shape pattern: two squares in one direction and one square in a perpendicular direction
+        if (row_diff == 2 and col_diff == 1) or (row_diff == 1 and col_diff == 2):
+            return True
+
+        return False
+
 
 class Bishop(Piece):
     def __init__(self, color):
         piece_id = color + "B"
         super().__init__(color, piece_id)
 
+    def is_valid_move(self, origin, target):
+        # TODO Write a method that returns True if the move is diagonal AND all spaces on the diagonal path are clear
+        return True
+
 class Rook(Piece):
     def __init__(self, color):
         piece_id = color + "R"
         super().__init__(color, piece_id)
+    
+    def is_valid_move(self, origin, target):
+        # Check if the move is valid for a rook
+
+        # Convert coordinates to indices (0-based)
+        origin_col, origin_row = ord(origin[0]) - ord('a'), 8 - int(origin[1])
+        target_col, target_row = ord(target[0]) - ord('a'), 8 - int(target[1])
+
+        # TODO adjust this code to check the spaces in between origin and target to ensure they are not occupied
+
+        if origin_row == target_row or origin_col == target_col:
+            return True
 
 class Pawn(Piece):
     def __init__(self, color):
         piece_id = color + "P"
         super().__init__(color, piece_id)
+
+    def is_valid_move(self, origin, target):
+        # Check if the move is valid for a pawn
+
+        # Convert coordinates to indices (0-based)
+        origin_col, origin_row = ord(origin[0]) - ord('a'), 8 - int(origin[1])
+        target_col, target_row = ord(target[0]) - ord('a'), 8 - int(target[1])
+
+        if self.color == 'W':
+            # White pawn moves forward one or two squares on its first move
+            if (
+                origin_col == target_col
+                and origin_row - target_row in (1, 2 if origin_row == 6 else 0)
+            ):
+                return True
+            # White pawn captures diagonally
+            if (
+                abs(origin_col - target_col) == 1
+                and origin_row - target_row == 1
+            ):
+                return True
+        else:
+            # Black pawn moves forward one or two squares on its first move
+            if (
+                origin_col == target_col
+                and target_row - origin_row in (1, 2 if origin_row == 1 else 0)
+            ):
+                return True
+            # Black pawn captures diagonally
+            if (
+                abs(origin_col - target_col) == 1
+                and target_row - origin_row == 1
+            ):
+                return True
+
+        return False
+
 
 class Board:
     def __init__(self):
@@ -117,15 +205,22 @@ class Game:
 
         # Perform the move logic
         piece = self.game_board.board[origin_row][origin_col]
-        if isinstance(piece, Piece) and piece.color == color:
-            # Check if the target position is empty
-            if not isinstance(self.game_board.board[target_row][target_col], Piece):
+        if isinstance(piece, Piece) and piece.color == color and piece.is_valid_move(origin, target):
+            # Check if the target position is occupied by opposing piece
+            if isinstance(self.game_board.board[target_row][target_col], Piece) and piece.color != self.game_board.board[target_row][target_col].color:
+                # target piece is taken, removed from board
+                self.game_board.board[target_row][target_col] = piece
+                self.game_board.board[origin_row][origin_col] = ' '
+                self.game_board.print_board()
+                return not turn
+            elif not isinstance(self.game_board.board[target_row][target_col], Piece):
                 # Move the piece to the target position
                 self.game_board.board[target_row][target_col] = piece
                 self.game_board.board[origin_row][origin_col] = ' '
                 self.game_board.print_board()
                 return not turn
             else:
+                # players cannot remove their own pieces
                 print("Invalid move. Target position is occupied.")
                 return turn
         else:
@@ -147,6 +242,9 @@ class Game:
         return True
     
     def get_piece_at_origin():
+        # TODO 
+        # Write a method that accepts one coordinate (e.g., 'a1') and returns the Piece.piece_id on that coordinate on the board.
+        # This is just for testing, and could potentially be used for check/checkmate checks
         pass
     
     def run(self):
